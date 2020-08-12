9e341ecf3cb711ed4872bd9025ac8bcd0670c8f5
CaddyDz <caddydz4@gmail.com>
2020-08-12 16:13:30 +0100

    Removed unused css
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,40 +5,40 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Learts – Handmade Shop eCommerce HTML Template</title>
+    <title>{{ config('app.name') }}</title>
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.png">
 
     <!-- CSS
 	============================================ -->
 
     <!-- Vendor CSS (Bootstrap & Icon Font) -->
-    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/vendor/font-awesome-pro.min.css">
-    <link rel="stylesheet" href="assets/css/vendor/themify-icons.css">
-    <link rel="stylesheet" href="assets/css/vendor/customFonts.css"> -->
+    {{-- <link rel="stylesheet" href="/assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/vendor/font-awesome-pro.min.css">
+    <link rel="stylesheet" href="/assets/css/vendor/themify-icons.css">
+    <link rel="stylesheet" href="/assets/css/vendor/customFonts.css"> --}}
 
     <!-- Plugins CSS (All Plugins Files) -->
-    <link rel="stylesheet" href="assets/css/plugins/select2.min.css">
-    <link rel="stylesheet" href="assets/css/plugins/perfect-scrollbar.css">
-    <link rel="stylesheet" href="assets/css/plugins/swiper.min.css">
-    <link rel="stylesheet" href="assets/css/plugins/nice-select.css">
-    <link rel="stylesheet" href="assets/css/plugins/ion.rangeSlider.min.css">
-    <link rel="stylesheet" href="assets/css/plugins/photoswipe.css">
-    <link rel="stylesheet" href="assets/css/plugins/photoswipe-default-skin.css">
-    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
-    <link rel="stylesheet" href="assets/css/plugins/slick.css"> -->
+    {{-- <link rel="stylesheet" href="/assets/css/plugins/select2.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/perfect-scrollbar.css">
+    <link rel="stylesheet" href="/assets/css/plugins/swiper.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" href="/assets/css/plugins/ion.rangeSlider.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/photoswipe.css">
+    <link rel="stylesheet" href="/assets/css/plugins/photoswipe-default-skin.css">
+    <link rel="stylesheet" href="/assets/css/plugins/magnific-popup.css">
+    <link rel="stylesheet" href="/assets/css/plugins/slick.css"> --}}
 
     <!-- Main Style CSS -->
-     <link rel="stylesheet" href="assets/css/style.css"> 
+     {{-- <link rel="stylesheet" href="/assets/css/style.css"> --}}
 
     <!-- Use the minified version files listed below for better performance and remove the files listed above -->
-    <link rel="stylesheet" href="assets/css/vendor/vendor.min.css">
-    <link rel="stylesheet" href="assets/css/plugins/plugins.min.css">
-    <link rel="stylesheet" href="assets/css/style.min.css">
+    <link rel="stylesheet" href="/assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/plugins.min.css">
+    <link rel="stylesheet" href="/assets/css/style.min.css">
 
 </head>
 
@@ -61,7 +61,7 @@
                 </div>-->
 
                 <!-- Header Language & Currency Start -->
-                <div class="col d-none d-md-block">
+                {{-- <div class="col d-none d-md-block">
                     <ul class="header-lan-curr text-white justify-content-end">
                         <li><a href="#">Français</a>
                             <ul class="curr-lan-sub-menu">
@@ -76,7 +76,7 @@
                             </ul> -->
                         </li>
                     </ul>
-                </div>
+                </div> --}}
                 <!-- Header Language & Currency End -->
             </div>
         </div>
