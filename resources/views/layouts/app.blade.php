e8ca46c72197558e32350e549052ee7f1500af9c
aicyassia <assia.benhadadi@gmail.com>
2020-09-02 20:07:29 +0100

    footer translation and modification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -990,9 +990,9 @@
 
 				<div class="col-xl-4 col-lg-5 col-12 learts-mb-40">
 					<div class="widget-contact">
-						<p class="email">contact@learts.com</p>
+						<p class="email">contact@dlaladz.com</p>
 						<p class="phone">(+88) 123 4566 6868</p>
-						<p class="copyright learts-mt-40">&copy; 2020 learts. All Rights Reserved</a></p>
+						<p class="copyright learts-mt-40">&copy; 2020 DlalaDz. All Rights Reserved</a></p>
 					</div>
 				</div>
 
@@ -1000,18 +1000,9 @@
 					<div class="row row-cols-sm-3 row-cols-1 learts-mb-n40">
 						<div class="col learts-mb-40">
 							<ul class="widget-list">
-								<li><a href="#">Men</a></li>
-								<li><a href="#">Women</a></li>
-								<li><a href="#">Accessories</a></li>
-								<li><a href="#">Flash sale</a></li>
-							</ul>
-						</div>
-						<div class="col learts-mb-40">
-							<ul class="widget-list">
-								<li><a href="#">About us</a></li>
-								<li><a href="#">Store location</a></li>
-								<li><a href="#">Contact</a></li>
-								<li><a href="#">Support Policy</a></li>
+								<li><a href="#">@lang('About us')</a></li>
+								<li><a href="#">@lang('Store location')</a></li>
+								<li><a href="#">@lang('Shipping policy')</a></li>
 								<li><a href="#">FAQs</a></li>
 							</ul>
 						</div>
@@ -1029,8 +1020,8 @@
 				<div class="col-xl-4 col-12 learts-mb-40">
 					<h5 class="widget-title mb-2">Newsletter</h5>
 					<form id="mc-form" class="mc-form widget-subscibe2">
-						<input id="mc-email" autocomplete="off" type="email" placeholder="Enter your e-mail address">
-						<button id="mc-submit" class="btn">subscibe</button>
+						<input id="mc-email" autocomplete="off" type="email" placeholder="Enterer votre addresse e-mail">
+						<button id="mc-submit" class="btn">@lang('subscibe')</button>
 					</form>
 					<!-- mailchimp-alerts Start -->
 					<div class="mailchimp-alerts text-centre">
