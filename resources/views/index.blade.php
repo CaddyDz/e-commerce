b8c540fde08262a45c527a745b56b389fbfdda76
aicyassia <assia.benhadadi@gmail.com>
2020-09-03 16:44:17 +0100

    banner change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,8 @@
 @section('content')
 	<!-- Slider main container Start -->
 	<div class="home6-slider section">
-		<div class="home6-slide-item" data-bg-image="assets/images/baner.jpg">
-			<div class="container">
-				
-			</div>
+		<div class="home6-slide-item" data-bg-image="/assets/images/baner.png">
+			<div class="container"></div>
 		</div>
 	</div>
 	<!-- Slider main container End -->
