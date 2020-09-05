16127c921c9908a04adfc9c3580e97fe0e098bb7
01Eva <assia.benhadadi@gmail.com>
2020-09-05 03:00:03 +0200

    shipping

--- resources/views/shipping.blade.php
+++ resources/views/shipping.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
     <!-- Page Title/Header Start -->
-    <div class="page-title-section section" data-bg-image="assets/images/bg/page-title-1.jpg">
+    <div class="page-title-section section" data-bg-image="">
         <div class="container">
             <div class="row">
                 <div class="col">
@@ -31,30 +31,27 @@
             <div class="row learts-mb-n30">
 
                 <div class="col-xl-3 col-lg-4 col-12 mr-auto learts-mb-30">
-                    <h1 class="fw-400">The difference when you shop Learts!</h1>
+                    <h1 class="fw-400">Quand vous faite vos course chez nous :</h1>
                 </div>
                 <div class="col-lg-8 col-12 learts-mb-30">
                     <div class="row learts-mb-n30">
 
                         <div class="col-md-6 col-12 learts-mb-30">
-                            <p class="text-heading fw-600 learts-mb-10">Free Shipping</p>
-                            <p>Once receiving your order, we will turn your products around in 3-5 business days.</p>
+                            <p class="text-heading fw-600 learts-mb-10">Paiement En Espèce à la livraison</p>
+                            <p>Le Client  paye en espèce à la livraison de la commande.  lorsque vous faites livrer vos articles à domicile.</p>
                         </div>
 
                         <div class="col-md-6 col-12 learts-mb-30">
-                            <p class="text-heading fw-600 learts-mb-10">Free Returns</p>
-                            <p>We accept returns for freshly purchased products within 7 days from the payment.</p>
+                            <p class="text-heading fw-600 learts-mb-10">Période de livraison</p>
+                            <p>de 3 à 7 jours.</p>
                         </div>
 
                         <div class="col-md-6 col-12 learts-mb-30">
-                            <p class="text-heading fw-600 learts-mb-10">Superb Quality</p>
-                            <p>We make commitments that the quality of our products will and always will be superb.</p>
+                            <p class="text-heading fw-600 learts-mb-10">Frais de livraison</p>
+                            <p>La liste des frais de livraison bientot disponible sur le site</p>
                         </div>
 
-                        <div class="col-md-6 col-12 learts-mb-30">
-                            <p class="text-heading fw-600 learts-mb-10">Free Wrapping</p>
-                            <p>Upon request, items bought as gifts from our store can receive free wrapping service.</p>
-                        </div>
+   
 
                     </div>
                 </div>
