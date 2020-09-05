5f96a60fb46fabbae4b85d00c49d6f463df1bca1
aicyassia <assia.benhadadi@gmail.com>
2020-09-05 23:25:55 +0100

    faq editing informations

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -40,11 +40,12 @@
 						<div class="accordion" id="faq-accordion">
 							
 								<div class="card-header">
-									<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#faq-accordion-2">Comment me faire rembourser ou retourner les produits en cas de problème ?</button>
+									<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#faq-accordion-2">Echange et retournement de produits</button>
 								</div>
 
 								<div id="faq-accordion-2" class="collapse" data-parent="#faq-accordion">
 									<div class="card-body">
+										<h5>Comment me faire rembourser ou retourner les produits en cas de problème ? </h5>
 										<p>Tout doit se faire avant le paiement, c’est-à-dire au moment de la livraison de votre colis
                                     Le CLIENT s’engage à vérifier sa commande lors de la livraison . En cas de non-conformité des articles (mauvais ou endommagés) nous conseillons au client de ne pas accepter la commande et de la laisser au livreur pour retour à nos entrepôts. Le livreur fera le constat du problème, le notera sur le bon de livraison et reprendra la totalité de la marchandise sans frais supplémentaire. C’est le seul cas de retour possible.</p>
 									</div>
@@ -52,11 +53,12 @@
 							</div>
 							<div class="card">
 								<div class="card-header">
-									<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#faq-accordion-3">Is the support period extendable?</button>
+									<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#faq-accordion-3">Qualité des produits </button>
 								</div>
 
 								<div id="faq-accordion-3" class="collapse" data-parent="#faq-accordion">
 									<div class="card-body">
+									    <h5> Quelle-garantie ai-je quant à la qualité des produits acheté en ligne?</h5>
 										<p>l’importateur et/ou le producteur officiel pour l’Algérie de tous les produits que nous vous proposons à la vente sur notre site.Ce partenariat privilégié avec les grandes Marques Internationales vous garantit la qualité des produits .</p>
 									</div>
 								</div>
