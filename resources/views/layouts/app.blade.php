655a66144c418593af3d2c6e0e91dba3d3db0f61
01Eva <assia.benhadadi@gmail.com>
2020-09-05 04:16:44 +0200

    more changes to shipping page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -192,9 +192,11 @@
 				<!-- Header Logo End -->
 
 				<!-- Search Start -->
+				
 				<div class="col d-none d-xl-block">
 					<nav class="site-main-menu justify-content-center">
 						<ul>
+						<!--
 							<li class="has-children"><a href="#"><span class="menu-text">Home</span></a>
 								<ul class="sub-menu mega-menu">
 									<li>
@@ -235,141 +237,14 @@
 									</li>
 								</ul>
 							</li>
-							<li class="has-children"><a href="#"><span class="menu-text">Shop</span></a>
-								<ul class="sub-menu mega-menu">
-									<li>
-										<a href="#" class="mega-menu-title"><span class="menu-text">SHOP PAGES</span></a>
-										<ul>
-											<li><a href="shop.html"><span class="menu-text">Shop No Sidebar</span></a></li>
-											<li><a href="shop-left-sidebar.html"><span class="menu-text">Shop Left Sidebar</span></a></li>
-											<li><a href="shop-right-sidebar.html"><span class="menu-text">Shop Right Sidebar</span></a></li>
-											<li><a href="shop-fullwidth-no-gutters.html"><span class="menu-text">Shop Fullwidth No Space</span></a></li>
-											<li><a href="shop-fullwidth.html"><span class="menu-text">Shop Fullwidth No Sidebar</span></a></li>
-											<li><a href="shop-fullwidth-left-sidebar.html"><span class="menu-text">Shop Fullwidth Left Sidebar</span></a></li>
-											<li><a href="shop-fullwidth-right-sidebar.html"><span class="menu-text">Shop Fullwidth Right Sidebar</span></a></li>
-										</ul>
-									</li>
-									<li>
-										<a href="#" class="mega-menu-title"><span class="menu-text">PRODUCT PAGES</span></a>
-										<ul>
-											<li><a href="product-details.html"><span class="menu-text">Basic</span></a></li>
-											<li><a href="product-details-fullwidth.html"><span class="menu-text">Fullwidth</span></a></li>
-											<li><a href="product-details-sticky.html"><span class="menu-text">Sticky Details</span></a></li>
-											<li><a href="product-details-sidebar.html"><span class="menu-text">Width Sidebar</span></a></li>
-											<li><a href="product-details-extra-content.html"><span class="menu-text">Extra Content</span></a></li>
-											<li><a href="product-details-image-variation.html"><span class="menu-text">Variations Images</span></a></li>
-											<li><a href="product-details-group.html"><span class="menu-text">Bought Together</span></a></li>
-											<li><a href="product-details-360.html"><span class="menu-text">Product 360</span></a></li>
-										</ul>
-									</li>
-									<li>
-										<a href="#" class="mega-menu-title"><span class="menu-text">PRODUCT & Other PAGES</span></a>
-										<ul>
-											<li><a href="product-details-background.html"><span class="menu-text">Product with Background</span></a></li>
-											<li><a href="shopping-cart.html"><span class="menu-text">Shopping Cart</span></a></li>
-											<li><a href="checkout.html"><span class="menu-text">Checkout</span></a></li>
-											<li><a href="order-tracking.html"><span class="menu-text">Order Tracking</span></a></li>
-											<li><a href="wishlist.html"><span class="menu-text">Wishlist</span></a></li>
-											<li><a href="login-register.html"><span class="menu-text">Customer Login</span></a></li>
-											<li><a href="my-account.html"><span class="menu-text">My Account</span></a></li>
-											<li><a href="lost-password.html"><span class="menu-text">Lost Password</span></a></li>
-										</ul>
-									</li>
-									<li class="align-self-center">
-										<a href="#" class="menu-banner"><img src="/assets/images/banner/menu-banner-2.png" alt="Shop Menu Banner"></a>
-									</li>
-								</ul>
-							</li>
-							<li class="has-children"><a href="#"><span class="menu-text">Project</span></a>
+-->
+                            <li class="has-children" class=pull-righ><span class="menu-text">        &nbsp; </span>
+							<li class="has-children" class=pull-righ><a href="#"><span class="menu-text">Pages</span></a>
 								<ul class="sub-menu">
-									<li><a href="portfolio-3-columns.html"><span class="menu-text">Portfolio 3 Columns</span></a></li>
-									<li><a href="portfolio-4-columns.html"><span class="menu-text">Portfolio 4 Columns</span></a></li>
-									<li><a href="portfolio-5-columns.html"><span class="menu-text">Portfolio 5 Columns</span></a></li>
-									<li><a href="portfolio-details.html"><span class="menu-text">Portfolio Details</span></a></li>
-								</ul>
-							</li>
-							<li class="has-children"><a href="#"><span class="menu-text">Elements</span></a>
-								<ul class="sub-menu mega-menu">
-									<li>
-										<a href="#" class="mega-menu-title"><span class="menu-text">Column One</span></a>
-										<ul>
-											<li><a href="elements-products.html"><span class="menu-text">Product Styles</span></a></li>
-											<li><a href="elements-products-tabs.html"><span class="menu-text">Product Tabs</span></a></li>
-											<li><a href="elements-product-sale-banner.html"><span class="menu-text">Product & Sale Banner</span></a></li>
-										</ul>
-									</li>
-									<li>
-										<a href="#" class="mega-menu-title"><span class="menu-text">Column Two</span></a>
-										<ul>
-											<li><a href="elements-category-banner.html"><span class="menu-text">Category Banner</span></a></li>
-											<li><a href="elements-team.html"><span class="menu-text">Team Member</span></a></li>
-											<li><a href="elements-testimonials.html"><span class="menu-text">Testimonials</span></a></li>
-										</ul>
-									</li>
-									<li>
-										<a href="#" class="mega-menu-title"><span class="menu-text">Column Three</span></a>
-										<ul>
-											<li><a href="elements-instagram.html"><span class="menu-text">Instagram</span></a></li>
-											<li><a href="elements-map.html"><span class="menu-text">Google Map</span></a></li>
-											<li><a href="elements-icon-box.html"><span class="menu-text">Icon Box</span></a></li>
-										</ul>
-									</li>
-									<li>
-										<a href="#" class="mega-menu-title"><span class="menu-text">Column Four</span></a>
-										<ul>
-											<li><a href="elements-buttons.html"><span class="menu-text">Buttons</span></a></li>
-											<li><a href="elements-faq.html"><span class="menu-text">FAQs / Toggles</span></a></li>
-											<li><a href="elements-brands.html"><span class="menu-text">Brands</span></a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							<li class="has-children"><a href="#"><span class="menu-text">Blog</span></a>
-								<ul class="sub-menu">
-									<li class="has-children"><a href="blog-right-sidebar.html"><span class="menu-text">Standard Layout</span></a>
-										<ul class="sub-menu">
-											<li><a href="blog-right-sidebar.html"><span class="menu-text">Right Sidebar</span></a></li>
-											<li><a href="blog-left-sidebar.html"><span class="menu-text">Left Sidebar</span></a></li>
-											<li><a href="blog-fullwidth.html"><span class="menu-text">Full Width</span></a></li>
-										</ul>
-									</li>
-									<li class="has-children"><a href="blog-grid-right-sidebar.html"><span class="menu-text">Grid Layout</span></a>
-										<ul class="sub-menu">
-											<li><a href="blog-grid-right-sidebar.html"><span class="menu-text">Right Sidebar</span></a></li>
-											<li><a href="blog-grid-left-sidebar.html"><span class="menu-text">Left Sidebar</span></a></li>
-											<li><a href="blog-grid-fullwidth.html"><span class="menu-text">Full Width</span></a></li>
-										</ul>
-									</li>
-									<li class="has-children"><a href="blog-list-right-sidebar.html"><span class="menu-text">List Layout</span></a>
-										<ul class="sub-menu">
-											<li><a href="blog-list-right-sidebar.html"><span class="menu-text">Right Sidebar</span></a></li>
-											<li><a href="blog-list-left-sidebar.html"><span class="menu-text">Left Sidebar</span></a></li>
-											<li><a href="blog-list-fullwidth.html"><span class="menu-text">Full Width</span></a></li>
-										</ul>
-									</li>
-									<li class="has-children"><a href="blog-masonry-right-sidebar.html"><span class="menu-text">Masonry Layout</span></a>
-										<ul class="sub-menu">
-											<li><a href="blog-masonry-right-sidebar.html"><span class="menu-text">Right Sidebar</span></a></li>
-											<li><a href="blog-masonry-left-sidebar.html"><span class="menu-text">Left Sidebar</span></a></li>
-											<li><a href="blog-masonry-fullwidth.html"><span class="menu-text">Full Width</span></a></li>
-										</ul>
-									</li>
-									<li class="has-children"><a href="blog-details-right-sidebar.html"><span class="menu-text">Single Post Layout</span></a>
-										<ul class="sub-menu">
-											<li><a href="blog-details-right-sidebar.html"><span class="menu-text">Right Sidebar</span></a></li>
-											<li><a href="blog-details-left-sidebar.html"><span class="menu-text">Left Sidebar</span></a></li>
-											<li><a href="blog-details-fullwidth.html"><span class="menu-text">Full Width</span></a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							<li class="has-children"><a href="#"><span class="menu-text">Pages</span></a>
-								<ul class="sub-menu">
-									<li><a href="about-us.html"><span class="menu-text">About us</span></a></li>
-									<li><a href="about-us-2.html"><span class="menu-text">About us 02</span></a></li>
-									<li><a href="contact-us.html"><span class="menu-text">Contact us</span></a></li>
-									<li><a href="coming-soon.html"><span class="menu-text">Coming Soon</span></a></li>
-									<li><a href="404.html"><span class="menu-text">Page 404</span></a></li>
+									<li><a href="/about"><span class="menu-text">About us</span></a></li>
+									<li><a href="/shipping"><span class="menu-text">@lang('Shipping policy')</span></a></li>
+									<li><a href="/faq"><span class="menu-text">FAQs</span></a></li>
+									
 								</ul>
 							</li>
 						</ul>
@@ -381,16 +256,17 @@
 				<div class="col-auto">
 					<div class="header-tools justify-content-end">
 						<div class="header-login">
-							<a href="my-account.html"><i class="fal fa-user"></i></a>
+							<a href="/home"><i class="fal fa-user"></i></a>
 						</div>
+						<!--
 						<div class="header-search d-none d-sm-block">
 							<a href="#offcanvas-search" class="offcanvas-toggle"><i class="fal fa-search"></i></a>
 						</div>
 						<div class="header-wishlist">
 							<a href="#offcanvas-wishlist" class="offcanvas-toggle"><span class="wishlist-count">3</span><i class="fal fa-heart"></i></a>
-						</div>
+						</div> -->
 						<div class="header-cart">
-							<a href="#offcanvas-cart" class="offcanvas-toggle"><span class="cart-count">3</span><i class="fal fa-shopping-cart"></i></a>
+							<a href="#offcanvas-cart" class="offcanvas-toggle"><span class="cart-count">{{ $cart->count() }}</span><i class="fal fa-shopping-cart"></i></a>
 						</div>
 						<div class="mobile-menu-toggle d-xl-none">
 							<a href="#offcanvas-mobile-menu" class="offcanvas-toggle">
@@ -436,7 +312,7 @@
 							<a href="#offcanvas-wishlist" class="offcanvas-toggle"><span class="wishlist-count">3</span><i class="fal fa-heart"></i></a>
 						</div>
 						<div class="header-cart">
-							<a href="#offcanvas-cart" class="offcanvas-toggle"><span class="cart-count">3</span><i class="fal fa-shopping-cart"></i></a>
+							<a href="#offcanvas-cart" class="offcanvas-toggle"><span class="cart-count">{{ $cart->count() }}</span><i class="fal fa-shopping-cart"></i></a>
 						</div>
 						<div class="mobile-menu-toggle">
 							<a href="#offcanvas-mobile-menu" class="offcanvas-toggle">
@@ -473,7 +349,7 @@
 				<div class="col-auto">
 					<div class="header-tools justify-content-end">
 						<div class="header-login d-none d-sm-block">
-							<a href="my-account.html"><i class="fal fa-user"></i></a>
+							<a href="/home"><i class="fal fa-user"></i></a>
 						</div>
 						<div class="header-search d-none d-sm-block">
 							<a href="#offcanvas-search" class="offcanvas-toggle"><i class="fal fa-search"></i></a>
@@ -482,7 +358,7 @@
 							<a href="#offcanvas-wishlist" class="offcanvas-toggle"><span class="wishlist-count">3</span><i class="fal fa-heart"></i></a>
 						</div>
 						<div class="header-cart">
-							<a href="#offcanvas-cart" class="offcanvas-toggle"><span class="cart-count">3</span><i class="fal fa-shopping-cart"></i></a>
+							<a href="#offcanvas-cart" class="offcanvas-toggle"><span class="cart-count">{{ $cart->count() }}</span><i class="fal fa-shopping-cart"></i></a>
 						</div>
 						<div class="mobile-menu-toggle">
 							<a href="#offcanvas-mobile-menu" class="offcanvas-toggle">
@@ -782,11 +658,10 @@
 					</li>
 					<li><a href="#"><span class="menu-text">Pages</span></a>
 						<ul class="sub-menu">
-							<li><a href="about-us.html"><span class="menu-text">About us</span></a></li>
+							<li><a href="/about"><span class="menu-text">@lang('About us')</span></a></li>
 							<li><a href="about-us-2.html"><span class="menu-text">About us 02</span></a></li>
 							<li><a href="contact-us.html"><span class="menu-text">Contact us</span></a></li>
-							<li><a href="coming-soon.html"><span class="menu-text">Coming Soon</span></a></li>
-							<li><a href="404.html"><span class="menu-text">Page 404</span></a></li>
+							
 						</ul>
 					</li>
 				</ul>
@@ -794,13 +669,13 @@
 			<div class="offcanvas-buttons">
 				<div class="header-tools">
 					<div class="header-login">
-						<a href="my-account.html"><i class="fal fa-user"></i></a>
+						<a href="/home"><i class="fal fa-user"></i></a>
 					</div>
 					<div class="header-wishlist">
 						<a href="wishlist.html"><span>3</span><i class="fal fa-heart"></i></a>
 					</div>
 					<div class="header-cart">
-						<a href="shopping-cart.html"><span class="cart-count">3</span><i class="fal fa-shopping-cart"></i></a>
+						<a href="shopping-cart.html"><span class="cart-count">{{ $cart->count() }}</span><i class="fal fa-shopping-cart"></i></a>
 					</div>
 				</div>
 			</div>
@@ -835,7 +710,7 @@
 						<div class="col learts-mb-40">
 							<ul class="widget-list">
 								<li><a href="/about">@lang('About us')</a></li>
-								<li><a href="#">@lang('Store location')</a></li>
+							
 								<li><a href="/shipping">@lang('Shipping policy')</a></li>
 								<li><a href="/faq">FAQs</a></li>
 							</ul>
