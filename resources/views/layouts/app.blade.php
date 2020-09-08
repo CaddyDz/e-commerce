a4b9dd1f131b50e5af79dc8a6f40682202e57c04
CaddyDz <caddydz4@gmail.com>
2020-09-08 23:47:02 +0100

    Quantity in cart
    * Closes #54
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -306,78 +306,6 @@
 		</div>
 	</div>
 	<!-- Mobile Header Section End -->
-	<!-- OffCanvas Search Start -->
-	<div id="offcanvas-search" class="offcanvas offcanvas-search">
-		<div class="inner">
-			<div class="offcanvas-search-form">
-				<button class="offcanvas-close">×</button>
-				<form action="#">
-					<div class="row mb-n3">
-						<div class="col-lg-8 col-12 mb-3"><input type="text" placeholder="Search Products..."></div>
-						<div class="col-lg-4 col-12 mb-3">
-							<select class="search-select select2-basic">
-								<option value="0">All Categories</option>
-								<option value="kids-babies">Kids &amp; Babies</option>
-								<option value="home-decor">Home Decor</option>
-								<option value="gift-ideas">Gift ideas</option>
-								<option value="kitchen">Kitchen</option>
-								<option value="toys">Toys</option>
-								<option value="kniting-sewing">Kniting &amp; Sewing</option>
-								<option value="pots">Pots</option>
-							</select>
-						</div>
-					</div>
-				</form>
-			</div>
-			<p class="search-description text-body-light mt-2"> <span># Type at least 1 character to search</span> <span># Hit enter to search or ESC to close</span></p>
-
-		</div>
-	</div>
-	<!-- OffCanvas Search End -->
-
-	<!-- OffCanvas Wishlist Start -->
-	<div id="offcanvas-wishlist" class="offcanvas offcanvas-wishlist">
-		<div class="inner">
-			<div class="head">
-				<span class="title">Wishlist</span>
-				<button class="offcanvas-close">×</button>
-			</div>
-			<div class="body customScroll">
-				<ul class="minicart-product-list">
-					<li>
-						<a href="product-details.html" class="image"><img src="/assets/images/product/cart-product-1.jpg" alt="Cart product Image"></a>
-						<div class="content">
-							<a href="product-details.html" class="title">Walnut Cutting Board</a>
-							<span class="quantity-price">1 x <span class="amount">$100.00</span></span>
-							<a href="#" class="remove">×</a>
-						</div>
-					</li>
-					<li>
-						<a href="product-details.html" class="image"><img src="/assets/images/product/cart-product-2.jpg" alt="Cart product Image"></a>
-						<div class="content">
-							<a href="product-details.html" class="title">Lucky Wooden Elephant</a>
-							<span class="quantity-price">1 x <span class="amount">$35.00</span></span>
-							<a href="#" class="remove">×</a>
-						</div>
-					</li>
-					<li>
-						<a href="product-details.html" class="image"><img src="/assets/images/product/cart-product-3.jpg" alt="Cart product Image"></a>
-						<div class="content">
-							<a href="product-details.html" class="title">Fish Cut Out Set</a>
-							<span class="quantity-price">1 x <span class="amount">$9.00</span></span>
-							<a href="#" class="remove">×</a>
-						</div>
-					</li>
-				</ul>
-			</div>
-			<div class="foot">
-				<div class="buttons">
-					<a href="wishlist.html" class="btn btn-dark btn-hover-primary">view wishlist</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- OffCanvas Wishlist End -->
 
 	<!-- OffCanvas Cart Start -->
 	<div id="offcanvas-cart" class="offcanvas offcanvas-cart">
@@ -390,11 +318,22 @@
 				<ul class="minicart-product-list">
 					@foreach($cart as $item)
 						<li>
-							<a href="{{ route('product', ['product' => $item->model]) }}" class="image"><img src="{{ secure_asset('storage/'. $item->model->image) }}" alt="@lang('Cart product Image')"></a>
+							<a href="{{ route('product', ['product' => $item->model]) }}" class="image">
+								<img src="{{ secure_asset('storage/'. $item->model->image) }}" alt="@lang('Cart product Image')">
+							</a>
 							<div class="content">
-								<a href="{{ route('product', ['product' => $item->model]) }}" class="title">{{ $item->model->name }}</a>
-								<span class="quantity-price">{{ $item->qty }} x <span class="amount">{{ $item->price }}</span></span>
-								<a href="{{ route('cart.remove', ['product' => $item->rowId]) }}" class="remove" onclick="event.preventDefault();document.getElementById('remove-cart-{{ $item->rowId }}').submit();">×</a>
+								<a href="{{ route('product', ['product' => $item->model]) }}" class="title">
+									{{ $item->model->name }}
+								</a>
+								<span class="quantity-price">
+									{{ $item->qty }} x
+									<span class="amount">
+										{{ $item->total }}
+									</span>
+								</span>
+								<a href="{{ route('cart.remove', ['product' => $item->rowId]) }}" class="remove" onclick="event.preventDefault();document.getElementById('remove-cart-{{ $item->rowId }}').submit();">
+									×
+								</a>
 								<form action="{{ route('cart.remove', ['product' => $item->rowId]) }}" method="post" style="display: none;" id="remove-cart-{{ $item->rowId }}">
 									@csrf
 								</form>
@@ -451,10 +390,10 @@
 						</div>
 						<div class="col learts-mb-40">
 							<ul class="widget-list">
-								
+
 								<li> <i class="fab fa-facebook-f"></i> <a href="https://www.facebook.com/">Facebook</a></li>
 								<li> <i class="fab fa-instagram"></i> <a href="https://www.instagram.com/">Instagram</a></li>
-								
+
 							</ul>
 						</div>
 					</div>
