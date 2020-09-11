c0b3a2aae35894fdabe3adc20ce83f5bb807b4d8
CaddyDz <caddydz4@gmail.com>
2020-09-11 03:25:36 +0100

    Hot fix shippings
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -7,79 +7,80 @@ use Illuminate\Http\Request;
 
 class ShippingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$shippings = Shipping::all();
+		return view('shipping', compact('shippings'));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Shipping  $shipping
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Shipping $shipping)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Shipping  $shipping
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Shipping $shipping)
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Shipping  $shipping
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Shipping $shipping)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Shipping  $shipping
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Shipping $shipping)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Shipping  $shipping
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Shipping $shipping)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Shipping  $shipping
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Shipping $shipping)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Shipping  $shipping
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Shipping $shipping)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Shipping  $shipping
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Shipping $shipping)
+	{
+		//
+	}
 }
