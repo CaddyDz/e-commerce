239e2106019aa564a3d99b5962d54b959bb204b4
aicyassia <assia.benhadadi@gmail.com>
2020-09-24 16:28:52 +0100

    419 error page

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', __('Page Not Found'))
+
+@section('content')
+<!-- 419 Section Start -->
+<div class="section-404 section" data-bg-image="/assets/images/bg/bg-404.jpg">
+	<div class="container">
+		<div class="">
+			<h1 class="title">Oops!</h1>
+			<h2 class="sub-title">Temps de réponse a expiré!</h2>
+			<p>@lang('You could either go back or go to homepage')</p>
+			<div class="buttons">
+				<a class="btn btn-primary btn-outline-hover-dark" href="{{ url()->previous() }}">
+					@lang('Go back')
+				</a>
+				<a class="btn btn-dark btn-outline-hover-dark" href="/">
+					@lang('Homepage')
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- 419 Section End -->
+@stop
