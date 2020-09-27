75890b4602abe57c978e13ee0e2da6bc1ebd0c4c
CaddyDz <caddydz4@gmail.com>
2020-09-27 12:28:19 +0100

    Localized user resource fields
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -50,24 +50,24 @@ class User extends Resource
 		return [
 			ID::make()->sortable(),
 
-			Text::make('Name')
+			Text::make(__('Name'), 'name')
 				->sortable()
 				->rules('required', 'max:255'),
 
-			Text::make('Email')
+			Text::make(__('Email'), 'email')
 				->sortable()
 				->rules('required', 'email', 'max:254')
 				->creationRules('unique:users,email')
 				->updateRules('unique:users,email,{{resourceId}}'),
 
-			Password::make('Password')
+			Password::make(__('Password'), 'password')
 				->onlyOnForms()
 				->creationRules('required', 'string', 'min:8')
 				->updateRules('nullable', 'string', 'min:8'),
 
 			Text::make(__('Address'), 'address'),
 
-			Text::make(__('Phone number'), 'phone'),
+			Text::make(__('Phone number'), 'phone')->displayUsing(fn($phone) => 0 . $phone),
 
 			RoleBooleanGroup::make('Roles'),
 
