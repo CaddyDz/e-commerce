43aa211034678b5feac0f0bebed7f0cc9a92533b
Salim Djerbouh <caddydz4@gmail.com>
2021-09-22 10:11:42 +0200

    Updated config
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,29 +1,38 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-	->notPath('bootstrap/cache')
-	->notPath('storage')
-	->notPath('vendor')
-	->notPath('node_modules')
-	->in(__DIR__)
-	->name('*.php')
-	->notName('*.blade.php')
-	->notName('_ide_helper.php')
-	->ignoreDotFiles(true)
-	->ignoreVCS(true);
-
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
 	->setRules([
-		'array_syntax' => ['syntax' => 'short'],
-		'indentation_type' => true,
-		'blank_line_after_opening_tag' => true,
+		'@PSR1' => true,
+		'use_arrow_functions' => true,
+		// presets tuning
+		'array_syntax' => [
+			'syntax' => 'short',
+		],
 		'declare_strict_types' => true,
-		'ordered_imports' => ['sort_algorithm' => 'length'],
-		'ordered_traits' => true,
+		'indentation_type' => true,
+		'blank_line_after_namespace' => true,
+		'blank_line_after_opening_tag' => true,
+		'single_blank_line_before_namespace' => true,
+		'single_line_after_imports' => true,
+		'no_blank_lines_after_class_opening' => true,
+		'visibility_required' => true,
 		'no_unused_imports' => true,
 		'group_import' => true,
-		'cast_spaces' => ['space' => 'single'],
-		'use_arrow_functions' => true,
-		'single_line_after_imports' => true,
-	])->setIndent("\t")
-	->setFinder($finder);
+		'ordered_imports' => ['sort_algorithm' => 'length'],
+		'ordered_traits' => true,
+		'cast_spaces' => ['space' => 'single']
+	])->setIndent("    ")
+	->setFinder(
+		(new PhpCsFixer\Finder())
+			->notPath('bootstrap/cache')
+			->notPath('storage')
+			->notPath('vendor')
+			->notPath('node_modules')
+			->in(__DIR__)
+			->append([__FILE__])
+			->name('*.php')
+			->notName('*.blade.php')
+			->notName('_ide_helper.php')
+			->ignoreDotFiles(true)
+			->ignoreVCS(true)
+	);
