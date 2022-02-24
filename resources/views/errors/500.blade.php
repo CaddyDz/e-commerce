164d4d2df70bdb4f77a7272b1402b96decd5a448
Salim Djerbouh <caddydz4@gmail.com>
2022-02-24 21:47:22 +0100

    Custom 500 error page
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Server Error</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.webp">
+    <link rel="stylesheet" href="/assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/plugins.min.css">
+    <link rel="stylesheet" href="/assets/css/style.min.css">
+
+</head>
+
+<body>
+
+    <div class="section-404 section" data-bg-image="/assets/images/bg/bg-404.webp">
+        <div class="container">
+            <div class="content-404">
+                <h1 class="title">Oops!</h1>
+                <h2 class="sub-title">Server Error</h2>
+                <p>We have been alerted and will fix it soon</p>
+                <p>You could either go back or go to homepage</p>
+                <div class="buttons">
+                    <a class="btn btn-primary btn-outline-hover-dark" href="javascript:history.back()">Go back</a>
+                    <a class="btn btn-dark btn-outline-hover-dark" href="/">Homepage</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="/assets/js/vendor/vendor.min.js"></script>
+    <script src="/assets/js/plugins/plugins.min.js"></script>
+
+    <!-- Main Activation JS -->
+    <script src="/assets/js/main.js"></script>
+
+</body>
+
+</html>
