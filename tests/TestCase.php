9dc4d56222e9f43cf1ef8c90d38274281444e0bc
CaddyDz <CaddyDz@users.noreply.github.com>
2022-02-24 19:39:18 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\{RefreshDatabase, TestCase as BaseTestCase};
 
 abstract class TestCase extends BaseTestCase
 {
